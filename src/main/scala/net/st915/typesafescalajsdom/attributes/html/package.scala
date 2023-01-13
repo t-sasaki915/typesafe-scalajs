3fa64e586beff8742de5461c0f69a1f0bda5eeb0
stouma915 <sasakitouma@yahoo.ne.jp>
2023-01-13 18:40:37 +0900

    Add package.scala

---
+++ src/main/scala/net/st915/typesafescalajsdom/attributes/html/package.scala
@@ -0,0 +1,29 @@
+package net.st915.typesafescalajsdom.attributes
+
+import net.st915.typesafescalajsdom.attributes.Attribute
+
+package object html {
+
+  sealed trait InputAttribute
+  sealed trait FormAttribute
+  sealed trait ImgAttribute
+  sealed trait ScriptAttribute
+  sealed trait ButtonAttribute
+  sealed trait SelectAttribute
+  sealed trait TextareaAttribute
+  sealed trait AudioAttribute
+  sealed trait VideoAttribute
+  sealed trait MetaAttribute
+  sealed trait TdAttribute
+  sealed trait ThAttribute
+  sealed trait OptionAttribute
+  sealed trait AnchorAttribute
+  sealed trait LabelAttribute
+  sealed trait CanvasAttribute
+  sealed trait EmbedAttribute
+  sealed trait IframeAttribute
+  sealed trait BodyAttribute
+  sealed trait AllVisibleAttribute
+  sealed trait DetailsAttribute
+
+}
