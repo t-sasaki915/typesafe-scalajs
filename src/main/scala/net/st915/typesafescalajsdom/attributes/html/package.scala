2cb7ac13f5bc7bd2492eb8a46e5f5df1f14431be
stouma915 <sasakitouma@yahoo.ne.jp>
2023-01-14 12:16:02 +0900

    Update package.scala

--- src/main/scala/net/st915/typesafescalajsdom/attributes/html/package.scala
+++ src/main/scala/net/st915/typesafescalajsdom/attributes/html/package.scala
@@ -4,26 +4,56 @@ import net.st915.typesafescalajsdom.attributes.Attribute
 
 package object html {
 
-  sealed trait InputAttribute
-  sealed trait FormAttribute
-  sealed trait ImgAttribute
-  sealed trait ScriptAttribute
-  sealed trait ButtonAttribute
-  sealed trait SelectAttribute
-  sealed trait TextareaAttribute
+  sealed trait VisibleElementAttribute
+  sealed trait AnchorAttribute extends VisibleElementAttribute
+  sealed trait AreaAttribute extends VisibleElementAttribute
   sealed trait AudioAttribute
-  sealed trait VideoAttribute
+  sealed trait BRAttribute extends VisibleElementAttribute
+  sealed trait BaseAttribute
+  sealed trait BodyAttribute extends VisibleElementAttribute
+  sealed trait ButtonAttribute extends VisibleElementAttribute
+  sealed trait CanvasAttribute extends VisibleElementAttribute
+  sealed trait DialogAttribute extends VisibleElementAttribute
+  sealed trait DivAttribute extends VisibleElementAttribute
+  sealed trait EmbedAttribute extends VisibleElementAttribute
+  sealed trait FieldSetAttribute extends VisibleElementAttribute
+  sealed trait FormAttribute extends VisibleElementAttribute
+  sealed trait HRAttribute extends VisibleElementAttribute
+  sealed trait HeadAttribute
+  sealed trait HTMLAttribute
+  sealed trait IFrameAttribute extends VisibleElementAttribute
+  sealed trait ImageAttribute extends VisibleElementAttribute
+  sealed trait InputAttribute extends VisibleElementAttribute
+  sealed trait LIAttribute extends VisibleElementAttribute
+  sealed trait LabelAttribute extends VisibleElementAttribute
+  sealed trait LegendAttribute extends VisibleElementAttribute
+  sealed trait MapAttribute extends VisibleElementAttribute
+  sealed trait MediaAttribute
+  sealed trait MenuAttribute extends VisibleElementAttribute
   sealed trait MetaAttribute
-  sealed trait TdAttribute
-  sealed trait ThAttribute
-  sealed trait OptionAttribute
-  sealed trait AnchorAttribute
-  sealed trait LabelAttribute
-  sealed trait CanvasAttribute
-  sealed trait EmbedAttribute
-  sealed trait IframeAttribute
-  sealed trait BodyAttribute
-  sealed trait AllVisibleAttribute
-  sealed trait DetailsAttribute
+  sealed trait OListAttribute extends VisibleElementAttribute
+  sealed trait ObjectAttribute extends VisibleElementAttribute
+  sealed trait OptGroupAttribute extends VisibleElementAttribute
+  sealed trait OptionAttribute extends VisibleElementAttribute
+  sealed trait ParagraphAttribute extends VisibleElementAttribute
+  sealed trait ParamAttribute
+  sealed trait PreAttribute extends VisibleElementAttribute
+  sealed trait ProgressAttribute extends VisibleElementAttribute
+  sealed trait QuoteAttribute extends VisibleElementAttribute
+  sealed trait ScriptAttribute
+  sealed trait SelectAttribute extends VisibleElementAttribute
+  sealed trait SourceAttribute
+  sealed trait SpanAttribute extends VisibleElementAttribute
+  sealed trait StyleAttribute
+  sealed trait TableCaptionAttribute extends VisibleElementAttribute
+  sealed trait TableCellAttribute extends VisibleElementAttribute
+  sealed trait TableColAttribute extends VisibleElementAttribute
+  sealed trait TableRowAttribute extends VisibleElementAttribute
+  sealed trait TableSectionAttribute extends VisibleElementAttribute
+  sealed trait TemplateAttribute extends VisibleElementAttribute
+  sealed trait TextAreaAttribute extends VisibleElementAttribute
+  sealed trait TitleAttribute
+  sealed trait TrackAttribute
+  sealed trait VideoAttribute extends VisibleElementAttribute
 
 }
