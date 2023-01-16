e519e464711f151ba61860464126d5dbc9a58238
stouma915 <sasakitouma@yahoo.ne.jp>
2023-01-16 13:17:01 +0900

    Update package.scala

--- src/main/scala/net/st915/typesafescalajsdom/dsl/package.scala
+++ src/main/scala/net/st915/typesafescalajsdom/dsl/package.scala
@@ -2,6 +2,7 @@ package net.st915.typesafescalajsdom
 
 package object dsl extends attributes.all with syntax.all {
 
+  final type TextNode = net.st915.typesafescalajsdom.TextNode
   final val TextNode = net.st915.typesafescalajsdom.TextNode
 
   final type Anchor = tags.Anchor
