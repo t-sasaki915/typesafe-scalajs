8934b8abcbb14d7ad658027be61ba9c67dfd1f0f
stouma915 <sasakitouma@yahoo.ne.jp>
2023-01-24 14:13:15 +0900

    Update HTMLApp

--- renderer/src/main/scala/net/st915/typesafescalajs/renderer/HTMLApp.scala
+++ renderer/src/main/scala/net/st915/typesafescalajs/renderer/HTMLApp.scala
@@ -9,7 +9,7 @@ trait HTMLApp extends IOApp {
   val headProgram: IO[Head]
   val bodyProgram: IO[Body]
 
-  override def run(args: List[String]): IO[ExitCode] = {
+  override final def run(args: List[String]): IO[ExitCode] = {
 
     import cats.effect.unsafe.implicits.global
     import net.st915.typesafescalajs.renderer.environments.global
