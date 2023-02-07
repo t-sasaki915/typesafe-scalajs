29837b2ef44a70655097b5251981a5ffdd456010
stouma915 <sasakitouma@yahoo.ne.jp>
2023-02-08 08:40:50 +0900

    Update package.scala

--- dom/src/main/scala/net/st915/typesafescalajs/dom/dsl/package.scala
+++ dom/src/main/scala/net/st915/typesafescalajs/dom/dsl/package.scala
@@ -1,8 +1,6 @@
 package net.st915.typesafescalajs.dom
 
-package object dsl extends attributes.all with predefvalues.all with syntax.all with tags.all {
-
-  final type TextNode = net.st915.typesafescalajs.dom.TextNode
-  final val TextNode = net.st915.typesafescalajs.dom.TextNode
-
-}
+package object dsl extends attributes.all
+    with predefvalues.all
+    with syntax.all
+    with tags.all
