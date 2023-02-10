4bdee767db5303ed959c0bf2a632e3837571ab35
stouma915 <sasakitouma@yahoo.ne.jp>
2023-02-10 10:38:52 +0900

    Add AcceptAttributes

---
+++ dom/src/main/scala/net/st915/typesafescalajs/dom/tags/AcceptAttributes.scala
@@ -0,0 +1,64 @@
+package net.st915.typesafescalajs.dom.tags
+
+import net.st915.typesafescalajs.dom.attributes.global.GlobalAttribute
+import net.st915.typesafescalajs.dom.attributes.html.*
+
+object AcceptAttributes {
+
+  type Anchor = GlobalAttribute | AnchorAttribute | VisibleElementAttribute
+  type Area = GlobalAttribute | AreaAttribute | VisibleElementAttribute
+  type Audio = GlobalAttribute | AudioAttribute
+  type BR = GlobalAttribute | BRAttribute | VisibleElementAttribute
+  type Base = GlobalAttribute | BaseAttribute
+  type Body = GlobalAttribute | BodyAttribute | VisibleElementAttribute
+  type Button = GlobalAttribute | ButtonAttribute | VisibleElementAttribute
+  type Canvas = GlobalAttribute | CanvasAttribute | VisibleElementAttribute
+  type Dialog = GlobalAttribute | DialogAttribute | VisibleElementAttribute
+  type Div = GlobalAttribute | DivAttribute | VisibleElementAttribute
+  type Embed = GlobalAttribute | EmbedAttribute | VisibleElementAttribute
+  type FieldSet = GlobalAttribute | FieldSetAttribute | VisibleElementAttribute
+  type Form = GlobalAttribute | FormAttribute | VisibleElementAttribute
+  type H1 = GlobalAttribute | HeadingAttribute | VisibleElementAttribute
+  type H2 = GlobalAttribute | HeadingAttribute | VisibleElementAttribute
+  type H3 = GlobalAttribute | HeadingAttribute | VisibleElementAttribute
+  type H4 = GlobalAttribute | HeadingAttribute | VisibleElementAttribute
+  type H5 = GlobalAttribute | HeadingAttribute | VisibleElementAttribute
+  type H6 = GlobalAttribute | HeadingAttribute | VisibleElementAttribute
+  type Head = GlobalAttribute | HeadAttribute
+  type HR = GlobalAttribute | HRAttribute | VisibleElementAttribute
+  type HTMLMap = GlobalAttribute | MapAttribute | VisibleElementAttribute
+  type HTMLObject = GlobalAttribute | ObjectAttribute | VisibleElementAttribute
+  type HTMLOption = GlobalAttribute | OptionAttribute | VisibleElementAttribute
+  type IFrame = GlobalAttribute | IFrameAttribute | VisibleElementAttribute
+  type Image = GlobalAttribute | ImageAttribute | VisibleElementAttribute
+  type Input = GlobalAttribute | InputAttribute | VisibleElementAttribute
+  type Label = GlobalAttribute | LabelAttribute | VisibleElementAttribute
+  type Legend = GlobalAttribute | LegendAttribute | VisibleElementAttribute
+  type Link = GlobalAttribute | LinkAttribute
+  type LI = GlobalAttribute | LIAttribute | VisibleElementAttribute
+  type Menu = GlobalAttribute | MenuAttribute | VisibleElementAttribute
+  type Meta = GlobalAttribute | MetaAttribute
+  type OList = GlobalAttribute | OListAttribute | VisibleElementAttribute
+  type OptGroup = GlobalAttribute | OptGroupAttribute | VisibleElementAttribute
+  type Paragraph = GlobalAttribute | ParagraphAttribute | VisibleElementAttribute
+  type Param = GlobalAttribute | ParamAttribute
+  type Pre = GlobalAttribute | PreAttribute | VisibleElementAttribute
+  type Progress = GlobalAttribute | ProgressAttribute | VisibleElementAttribute
+  type Quote = GlobalAttribute | QuoteAttribute | VisibleElementAttribute
+  type Script = GlobalAttribute | ScriptAttribute
+  type Select = GlobalAttribute | SelectAttribute | VisibleElementAttribute
+  type Source = GlobalAttribute | SourceAttribute
+  type Span = GlobalAttribute | SpanAttribute | VisibleElementAttribute
+  type Style = GlobalAttribute | StyleAttribute
+  type Table = GlobalAttribute | TableAttribute | VisibleElementAttribute
+  type TableCaption = GlobalAttribute | TableCaptionAttribute | VisibleElementAttribute
+  type TableCell = GlobalAttribute | TableCellAttribute | VisibleElementAttribute
+  type TableCol = GlobalAttribute | TableColAttribute | VisibleElementAttribute
+  type TableRow = GlobalAttribute | TableRowAttribute | VisibleElementAttribute
+  type TableSection = GlobalAttribute | TableSectionAttribute | VisibleElementAttribute
+  type TextArea = GlobalAttribute | TextAreaAttribute | VisibleElementAttribute
+  type Title = GlobalAttribute | TitleAttribute
+  type Track = GlobalAttribute | TrackAttribute
+  type Video = GlobalAttribute | VideoAttribute | VisibleElementAttribute
+
+}
