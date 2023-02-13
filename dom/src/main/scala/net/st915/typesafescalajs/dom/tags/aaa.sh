8bcfe71355b17ed1daba8ecbb5c3953c245bf002
stouma915 <sasakitouma@yahoo.ne.jp>
2023-02-13 13:03:45 +0900

    Remove unused

--- dom/src/main/scala/net/st915/typesafescalajs/dom/tags/aaa.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-echo ""
-echo "  opaque type $1 = Tags.$1"
-echo "  object $1:"
-echo "    def apply(attrs: (AcceptAttributes.$1, _)*)(children: Node*): $1 ="
-echo "      Tags.$1(attrs.toSet, children.toList)"
-echo "    def apply(values: ((AcceptAttributes.$1, _) | Node)*): $1 ="
-echo "      applyMacro(Tags.$1.apply)(values)"
