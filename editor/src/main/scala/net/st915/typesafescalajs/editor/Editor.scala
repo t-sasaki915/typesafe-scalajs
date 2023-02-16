497672c8ffa5efe0a10e75c1e885e3841e52b099
stouma915 <sasakitouma@yahoo.ne.jp>
2023-02-16 11:10:37 +0900

    Update Editor.scala

--- editor/src/main/scala/net/st915/typesafescalajs/editor/Editor.scala
+++ editor/src/main/scala/net/st915/typesafescalajs/editor/Editor.scala
@@ -1,3 +1,19 @@
 package net.st915.typesafescalajs.editor
 
-trait Editor
+import cats.data.Kleisli
+import cats.effect.Sync
+import net.st915.typesafescalajs.dom.Environment
+import net.st915.typesafescalajs.editor.typeclasses.*
+import org.scalajs.dom.HTMLElement
+
+case class Editor[F[_]: Sync]()(using Environment) {
+
+  import net.st915.typesafescalajs.editor.instances.all.given
+
+  val getNativeElementById: Kleisli[F, String, Option[HTMLElement]] =
+    CanGetNativeElementById[F].getNativeElementById
+
+  val getNativeElementsByClassName: Kleisli[F, String, List[HTMLElement]] =
+    CanGetNativeElementsByClassName[F].getNativeElementsByClassName
+
+}
